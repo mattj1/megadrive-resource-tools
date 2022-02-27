c97022def0bc8b1bb6e1e232ff17fbf4f752c151
Matt Johnson <matt.johnson.dev@gmail.com>
2022-02-27 16:12:07 -0700

    various

--- megadrive_resource_tools/restool.py
+++ megadrive_resource_tools/restool.py
@@ -9,8 +9,55 @@ def get_file(file_name):
 
 
 class Tileset:
-    def __init__(self, name):
+    def __init__(self, name, restool):
         self.name = name
+        self.restool = restool
+        self.num_tiles = 0
+        self.start_tile = 0
+
+        self.restool.write_tile(f"/* Tileset: TILESET_{self.name} */")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("Tileset close: {}".format(self.name))
+        print("number of tiles: {}".format(self.num_tiles))
+
+    def transfer_region(self, sprite_src_x, sprite_src_y, sprite_tile_w, sprite_tile_h):
+        # width and height are in tiles
+
+        for x in range(0, sprite_tile_w):
+            for y in range(0, sprite_tile_h):
+                self.num_tiles += 1
+                self.restool.num_tiles += 1
+
+                # print(x, y, sprite_tile_w, sprite_tile_h)
+                left = sprite_src_x + x * 8
+                top = sprite_src_y + y * 8
+                r = (left, top, left + 8, top + 8)
+                region = self.restool.current_img.crop(r)
+                val = 0
+                # print(img_data, len(img_data))
+                for pixel_idx, pixel in enumerate(region.getdata()):
+
+                    try:
+                        idx = self.restool.pal_data.index(pixel)
+                    except ValueError:
+                        print("Warning: Color {} not found in palette".format(pixel))
+                        idx = 0
+
+                    val = (val << 4) | idx
+
+                    # sys.stdout.write("{}, ".format(hex(idx)))
+
+                    if (pixel_idx + 1) % 8 == 0:
+                        s = "dc.l 0x{:08X}".format(val, 10)
+                        # s = "dc.l ${0:#0{1}x}".format(val, 10)
+                        self.restool.write_tile(s)
+                        # if pixel_idx != len(img_data)-1:
+                        # write_out(s)
+                        val = 0
 
 
 class ResTool:
@@ -25,11 +72,13 @@ class ResTool:
         self.header_file = open("{}/res_generated.h".format(self.src_files_directory), "w")
         self.res_file = open("{}/res_generated.c".format(self.src_files_directory), "w")
 
-        self.write_res("#include")
+        self.write_res("#include \"res_generated.h\"")
 
-        self.tile_sets = []
+        self.tile_sets: [Tileset] = []
 
         self.total_tile_index = 0
+
+        self.num_tiles = 0
 
         self.write_tile(".align 2")
         self.write_tile(".globl TILES")
@@ -39,6 +88,31 @@ class ResTool:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+
+        self.write_header(f"#include <stdint.h>")
+
+        for i in range(0, len(self.tile_sets)):
+            ts = self.tile_sets[i]
+            self.write_header(f"#define TILESET_{ts.name} {i}")
+
+        self.write_header(f"""
+            typedef struct tileset_s tileset_t;
+            
+            struct tileset_s {{
+                int start_tile;
+                int num_tiles;
+            }};
+            
+            extern tileset_t tile_sets[{len(self.tile_sets)}];
+        """)
+
+        self.write_res(f"tileset_t tile_sets[{len(self.tile_sets)}] = {{")
+
+        for t in self.tile_sets:
+            self.write_res(f"{{ {t.start_tile}, {t.num_tiles} }}, // {t.name}")
+
+        self.write_res(f"}};")
+
         print("closing...")
         self.tile_file.close()
         self.header_file.close()
@@ -52,6 +126,9 @@ class ResTool:
 
     def write_res(self, s):
         self.res_file.write("{}\n".format(s))
+
+    def write_header(self, s):
+        self.header_file.write("{}\n".format(s))
 
     def use_palette(self, pal):
         self.pal_data = pal
@@ -73,43 +150,12 @@ class ResTool:
         self.use_image(Image.open(path))
 
     def begin_tileset(self, name) -> Tileset:
-        tileset = Tileset(name)
+        tileset = Tileset(name, self)
+        tileset.start_tile = self.num_tiles
         # self.current_set = {"name": name, "num_tiles": 0, "tile_no": self.total_tile_index}
         self.tile_sets.append(tileset)
         tile_index = 0
         return tileset
-
-    def transfer_region(self, sprite_src_x, sprite_src_y, sprite_tile_w, sprite_tile_h):
-        # width and height are in tiles
-
-        for x in range(0, sprite_tile_w):
-            for y in range(0, sprite_tile_h):
-                # print(x, y, sprite_tile_w, sprite_tile_h)
-                left = sprite_src_x + x * 8
-                top = sprite_src_y + y * 8
-                r = (left, top, left + 8, top + 8)
-                region = self.current_img.crop(r)
-                val = 0
-                # print(img_data, len(img_data))
-                for pixel_idx, pixel in enumerate(region.getdata()):
-
-                    try:
-                        idx = self.pal_data.index(pixel)
-                    except ValueError:
-                        print("Warning: Color {} not found in palette".format(pixel))
-                        idx = 0
-
-                    val = (val << 4) | idx
-
-                    # sys.stdout.write("{}, ".format(hex(idx)))
-
-                    if (pixel_idx + 1) % 8 == 0:
-                        s = "dc.l 0x{:08X}".format(val, 10)
-                        # s = "dc.l ${0:#0{1}x}".format(val, 10)
-                        self.write_tile(s)
-                        # if pixel_idx != len(img_data)-1:
-                        # write_out(s)
-                        val = 0
 
     # def begin_state(self, name):
     #     current_state = {"name": name, "sprites": ["", "", "", ""], "set": self.current_set["name"]}
--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,22 @@
+import PIL.Image
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
 from megadrive_resource_tools.restool import ResTool
 
+letter_dim = 16
+font = ImageFont.truetype(r'pixel font-7.ttf', 20)
+# img = Image.new('RGB', (letter_dim, letter_dim), color=(0, 255, 255))
+# img2 = Image.new('RGB', (letter_dim, letter_dim), color=(0, 255, 255))
+# draw = ImageDraw.Draw(img)
+# draw.text((0, 0), 'A', (255, 255, 255), font=font)
+# img = img.resize((8, 16), resample=PIL.Image.NEAREST)
+# img2.paste(img)
+# img2.show()
+# exit()
 with ResTool() as restool:
-    letter_dim = 16
+
 
     # restool.use_palette_from_image('/Users/mattj/projects/wordle-md/dev/tiles.png')
     pal = restool.generate_empty_palette()
@@ -17,29 +28,47 @@ with ResTool() as restool:
     pal[15] = (255, 255, 255)
     restool.use_palette(pal)
 
-    restool.begin_tileset('MAIN')
+    # restool.use_image_file('/Users/mattj/projects/wordle-md/dev/tiles.png')
+    restool.use_image_file('c:/Users/matt/Documents/GitHub/wordle-md/dev/tiles.png')
 
-    restool.use_image_file('/Users/mattj/projects/wordle-md/dev/tiles.png')
+    with restool.begin_tileset('CURSOR') as tileset:
+        # Cursor
+        tileset.transfer_region(0, 16, 2, 2)
+    # Gray, orange, green, empty
+    with restool.begin_tileset('WORD_TILES_FILLED') as tileset:
+        for i in range(0, 4):
+            tileset.transfer_region(0, 32 + 24 * i, 3, 3)
 
-    # Cursor
-    restool.transfer_region(0, 16, 2, 2)
+    with restool.begin_tileset('WORD_TILES_OUTLINE') as tileset:
+        for i in range(0, 4):
+            tileset.transfer_region(24, 32 + 24 * i, 3, 3)
 
-    for i in range(0, 4):
-        # Gray, orange, green, empty
-        restool.transfer_region(32 + 24 * i, 8, 3, 3)
+    with restool.begin_tileset('MISC') as tileset:
+        tileset.transfer_region(0, 0, 1, 1)
 
-    # font = ImageFont.truetype(r'gameovercre1.ttf', 16)
-    font = ImageFont.truetype(r'pixel font-7.ttf', 20)
+    sizes = ((16, 16), (12, 16), (8, 16), (4, 16))
+    with restool.begin_tileset('LARGE_FONT') as tileset:
+        # font = ImageFont.truetype(r'gameovercre1.ttf', 16)
 
-    for i in range(ord('A'), ord('Z') + 1):
-        # print(chr(i))
-        img = Image.new('RGB', (letter_dim, letter_dim), color=(0, 255, 255))
-        draw = ImageDraw.Draw(img)
+        for i in range(ord('A'), ord('Z') + 1):
+            # print(chr(i))
+            img = Image.new('RGB', (letter_dim, letter_dim), color=(0, 255, 255))
+            draw = ImageDraw.Draw(img)
 
-        bb = draw.textbbox(xy=(0, 0), text=chr(i), font=font)
-        draw.text((letter_dim/2 - bb[2] / 2, letter_dim/2 - bb[3] / 2), chr(i), (255, 255, 255), font=font)
+            bb = draw.textbbox(xy=(0, 0), text=chr(i), font=font)
+            draw.text((letter_dim/2 - bb[2] / 2, letter_dim/2 - bb[3] / 2), chr(i), (255, 255, 255), font=font)
 
-        restool.use_image(img)
-        restool.transfer_region(0, 0, 2, 2)
+            for size in sizes:
+                img2 = Image.new('RGB', (letter_dim, letter_dim), color=(0, 255, 255))
+                letter_resized = img.resize(size=size, resample=PIL.Image.NEAREST)
+                img2.paste(letter_resized, (int(letter_dim/2 - size[0]/2), 0))
 
-    img.show()
+                restool.use_image(img2)
+                tileset.transfer_region(0, 0, 2, 2)
+
+                # img2.show()
+
+                # letter_resized.show()
+            # exit()
+
+    # img.show()
